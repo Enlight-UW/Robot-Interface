20caecf271fd96b81f7d43f1e8d0748091b72023
Tom Schutte <tjschutte@wisc.edu>
2017-02-07 11:32:37 -0600

    Initial commit of node site
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Robot-Interface
 Web page to queue up a series for serial commands before sending them to an Arduino robot
+## installation
+1. Make sure you have Node and NPM installed for your OS
+2. Clone the repository, and go into the 'Robot-Interface' Directory
+3. Run `npm install` to install the required node modules
---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+ * Module dependencies.
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+const serialport = require('serialport');
+
+/**
+ * Port for sending serial data on a raspberry pi
+ * we will get more into this later
+ */
+//const port = new serialport('/dev/ttyUSB0', 9600);
+
+
+/**
+ * Create Express server.
+ */
+const app = express();
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+// ejs is something we will use later to make the html better
+app.engine('html', require('ejs').renderFile);
+
+/** bodyParser.urlencoded(options)
+* Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+* and exposes the resulting object (containing the keys and values) on req.body
+*/
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+/**bodyParser.json(options)
+* Parses the text as JSON and exposes the resulting object on req.body.
+*/
+app.use(bodyParser.json());
+
+/**
+ * Primary app routes.
+ */
+app.get('/', function getIndex(req, res) {
+  console.log('GET: /index');
+  res.render('index.html');
+});
+
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
+
+module.exports = app;
